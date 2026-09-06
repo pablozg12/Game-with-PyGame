b85bc942cf23e84e79d150fdae445ff314329dde
pablozg12 <pablozg120@gmail.com>
2026-09-06 10:12:10 -0700

    Stars, aliens and improvement
    I worked on the stars of the background for the visuals, i also put some "aliens" to begin with (they`re green squares but later on i`ll put them some images), and i fixed the logic of the keyboard so that the spaceship can only move if the player clicks the right or left key

--- MyGame/game.py
+++ MyGame/game.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import random
 
 display_width = 800
 display_height = 600
@@ -11,36 +13,80 @@ green = (0,255,0)
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption("A race")
+pygame.display.set_caption("A Space Adventure")
 clock = pygame.time.Clock()
 
 shippImg = pygame.image.load('spaceShip.png')
-widthImg = 170
-heightImg = 170
+widthImg = 100
+heightImg = 185
+
+crashed = False
 
 ship = pygame.transform.scale(shippImg, (widthImg,heightImg))
+
+def stars(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+def alien(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 def spaceShip(x,y):
     gameDisplay.blit(ship,(x,y))
 
+def text_objects(text,font):
+    textSurface = font.render(text, True, white)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font("freesansbold.ttf",100)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf,TextRect)
+    pygame.display.update()
+    time.sleep(1)
+    game_loop()
+
+def crash():
+    message_display('You Crashed!!!')    
 
 def game_loop():
-    x = (display_width * 0.38)
+    x = (display_width * 0.42)
     y = (display_height * 0.7)
 
     x_sum = 0
+
+    num_stars = 50 #Number of stars
+    stars_List = [] #Star's list
+    for _ in range(num_stars):
+        star_x = random.randrange(0,display_width)
+        star_y = random.randrange(0, display_height)
+        star_speed = random.randint(5,9) #Different speed
+        stars_List.append([star_x,star_y, star_speed])
+
+    thing_width = 5
+    thing_height = 5
+
+    alien_speed = 7
+    alien_startx = random.randrange(0,display_width)
+    alien_starty = -600
+    alien_width = 100
+    alien_height = 100
+
     gameExit = False
 
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_sum = -5
-                else:
+                elif event.key == pygame.K_RIGHT:
                     x_sum = 5
+                else:
+                    x_sum = 0
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_sum = 0
@@ -49,10 +95,28 @@ def game_loop():
         x += x_sum
 
         gameDisplay.fill(black)
+
+        alien(alien_startx, alien_starty, alien_width,alien_height, green)
+        alien_starty += alien_speed
+
+        #Loop for the stars on the background
+        for star in stars_List:
+            stars(star[0],star[1], thing_width, thing_height, white)
+
+            star[1] += star[2]
+
+            if star[1] > display_height:
+                star[1] = 0 - thing_height
+                star[0] = random.randrange(0, display_width)
+
+        if alien_starty > display_height:
+            alien_starty = 0 - alien_height
+            alien_startx = random.randrange(0, display_width)
+
         spaceShip(x,y)
 
         if x > display_width - widthImg or x < 0:
-            gameExit = True
+            crash()
 
         pygame.display.update() #Updates the parameters on the method
         clock.tick(60) #fps (frames per second)
