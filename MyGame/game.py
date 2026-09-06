d4d30d7e52f2f0327ce089096a346dbb1a84b110
pablozg12 <pablozg120@gmail.com>
2026-09-05 22:04:17 -0700

    Base structure of my game using pygame

--- MyGame/game.py
+++ MyGame/game.py
@@ -1,5 +1,20 @@
 import pygame
 
 pygame.init()
-
 gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption("A race")
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+        print(event)
+
+    pygame.display.update() #Updates the parameters on the method
+    clock.tick(60) #fps (frames per second)
+
+pygame.quit()
+quit()
